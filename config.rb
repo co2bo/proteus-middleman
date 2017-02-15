0c9c2cb6ab81ed87005c36f15fdcf07972bd4df7
Mathias Davidheimann <davidheimann5777@gmail.com>
2017-02-15 22:14:42 +0100

    Update config.rb

--- config.rb
+++ config.rb
@@ -1,26 +1,45 @@
-activate :directory_indexes
 activate :autoprefixer
+activate :directory_indexes
 
 set :relative_links, true
-set :css_dir, "assets/stylesheets"
-set :js_dir, "assets/javascripts"
-set :images_dir, "assets/images"
-set :fonts_dir, "assets/fonts"
-set :layout, "layouts/application"
 
-page '/*.xml', layout: false
+set :css_dir,    'assets/stylesheets'
+set :fonts_dir,  'assets/fonts'
+set :images_dir, 'assets/images'
+set :js_dir,     'assets/javascripts'
+
+set :layout, 'layouts/application'
+
+page '/*.xml',  layout: false
 page '/*.json', layout: false
-page '/*.txt', layout: false
+page '/*.txt',  layout: false
+
+#----------------------------------------------------
+# Development-specific Configuration
+#----------------------------------------------------
 
 configure :development do
-  activate :livereload
+  activate :livereload do |reload|
+    reload.no_swf = true
+  end
 end
 
-configure :build do
-  activate :relative_assets
+#----------------------------------------------------
+# Production-specific Configuration
+#----------------------------------------------------
+
+configure :production do
+  activate :gzip
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
 end
+
+#----------------------------------------------------
+# Deploy-specific Configuration
+#----------------------------------------------------
 
 activate :deploy do |deploy|
-  deploy.build_before = true
+  deploy.build_before  = true
   deploy.deploy_method = :git
 end
